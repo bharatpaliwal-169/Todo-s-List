d0626a35bb069add36fc7fdf5dc8656c043f29aa
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-06-29 11:05:32 +0530

    seo,pwa,modification added : web-app V2 updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -91,7 +91,7 @@ export default function Main() {
             
             <form className='form text-center' onSubmit={handleSubmit}>
 
-              <h4 className='display-5 fw-3  mb-4 headp'> Shopping Bud </h4>
+              <h4 className='headp'> Tasks List </h4>
               <TextField id="item"
                           label="add your item"
                           variant="outlined"
@@ -99,7 +99,9 @@ export default function Main() {
                           size="small"
                           value={name}
                           onChange={(e)=> setName(e.target.value)}/>
-              <Button variant='contained' type='submit' color='primary' size="medium"> {isEditing ? 'Edit' : 'Add'} </Button>
+              <Button variant='contained' type='submit' color='primary' size="medium" className='ml-4'>
+                {isEditing ? 'Edit' : 'Add'} 
+              </Button>
 
               {list.length > 0 && (
                       <div className='mb-2'>
